db112ea1064088c41bb51ad30240bac778c7257e
basma-1 <basmaashoura2@gmail.com>
2026-09-01 12:10:40 +0300

    feat: solve -Bean Counting- exercise pt1

--- js-log/functions.js
+++ js-log/functions.js
@@ -8,11 +8,23 @@
 // console.log(min(5, 5));
 
 // 2. RECURSION
-function isEven(testNum) {
-    if (testNum === 0) return true;
-    if (testNum === 1) return false;
-    if (testNum < 0) return isEven(testNum + 2);
-    return isEven(testNum - 2);
+// function isEven(testNum) {
+//     if (testNum === 0) return true;
+//     if (testNum === 1) return false;
+//     if (testNum < 0) return isEven(testNum + 2);
+//     return isEven(testNum - 2);
+// }
+//
+// console.log(isEven(-1))
+
+// 3. Bean Counting
+function countBs(testText) {
+    let counter = 0
+    for(let char of testText){
+        if (char === "B")
+            counter++
+    }
+    return counter;
 }
 
-console.log(isEven(-1))
+console.log(countBs("BoBBBBBBBBBBBBBB"))
