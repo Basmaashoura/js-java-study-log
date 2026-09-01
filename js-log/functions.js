c8ae4b35e09bc804335d9b53d9807aeeec23e370
basma-1 <basmaashoura2@gmail.com>
2026-09-01 11:59:09 +0300

    feat: solve -Recursion- exercise

--- js-log/functions.js
+++ js-log/functions.js
@@ -1,5 +1,18 @@
-function min(a, b) {
-  return a < b ? a : b;
+// CHAPTER EXERCISES SOLUTIONS
+//
+// 1. MINIMUM
+// function min(a, b) {
+//   return a < b ? a : b;
+// }
+//
+// console.log(min(5, 5));
+
+// 2. RECURSION
+function isEven(testNum) {
+    if (testNum === 0) return true;
+    if (testNum === 1) return false;
+    if (testNum < 0) return isEven(testNum + 2);
+    return isEven(testNum - 2);
 }
 
-console.log(min(5, 5));
+console.log(isEven(-1))
