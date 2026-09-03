197e6d41be57cfa88bc9662ce85375b07fec5677
basma-1 <basmaashoura2@gmail.com>
2026-09-03 17:10:30 +0300

    feat: solve ch.4 -Pool Puzzle- exercise

--- java-log/howObjectsBehave.java
+++ java-log/howObjectsBehave.java
@@ -2,7 +2,7 @@ public class howObjectsBehave {
     public static void main(String[] args) {
 
         // Be the compiler
-        {
+        /*{
         // ============ A ============
             int orig = 42;
             XCopy x = new XCopy();
@@ -14,6 +14,40 @@ public class howObjectsBehave {
             c.setTime("12345");
             String tod = c.getTime();
             System.out.println("time: " + tod);
+        }*/
+
+        // Pool Puzzle
+        {
+            Value[] values = new Value[6];
+            int number = 1;
+            int i = 0;
+            while (i < 6) {
+                values[i] = new Value();
+                values[i].intValue = number;
+                number = number * 10;
+                i = i + 1;
+            }
+
+            int result = 0;
+            i = 6;
+            while (i > 0) {
+                i = i - 1;
+                result = result + values[i].doStuff(i);
+                ;
+            }
+            System.out.println("result " + result);
+        }
+    }
+}
+
+
+class Value {
+    int intValue;
+    public int doStuff(int factor) {
+        if(intValue > 100) {
+            return intValue * factor;
+        } else {
+            return intValue * (5 - factor);
         }
     }
 }
