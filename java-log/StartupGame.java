818f0174b9efba618e6be27d10ba3456d5b66b0a
basma-1 <basmaashoura2@gmail.com>
2026-09-15 17:47:43 +0300

    feat: combine StartUpGame code snippets

---
+++ java-log/StartupGame.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class StartupGame {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+
+         GameHelper helper = new GameHelper();
+
+        simpleStartup theStartup = new simpleStartup();
+        int randomNum = (int) (Math.random()*5);
+        int[] locations = {randomNum, randomNum+1, randomNum+2};
+        theStartup.setLocationCells(locations);
+
+        boolean isAlive = true;
+
+        while (isAlive) {
+          int guess = helper.getUserInput("enter a number");
+            String result = theStartup.checkYourself(guess);
+            numOfGuesses++;
+            if(result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took " +numOfGuesses + " guesses");}
+            }
+        }
+}
+
+
+class simpleStartup {
+    int[] locationCells;
+    int numOfHits = 0;
+
+    String checkYourself(int guess) {
+        String result = "miss";
+
+        for(int cell : locationCells) {
+            if (cell == guess) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+
+        if(numOfHits == locationCells.length) {
+            result = "kill";
+        }
+
+        System.out.println(result);
+        return result;
+    }
+
+    void setLocationCells(int[] locs) {
+        locationCells = locs;
+    }
+}
+
+class GameHelper {
+    public int getUserInput(String prompt) {
+        System.out.print(prompt + ": ");
+        Scanner scanner = new Scanner(System.in);
+        return  scanner.nextInt();
+    }
+}
